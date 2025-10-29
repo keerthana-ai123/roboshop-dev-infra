7d7bc38252a3c6e839360d9c409c1e48f2f5e0f8
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-10-29 17:11:29 +0530

    commit

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 dnf install ansible -y
 # ansible-pull -U https://github.com/keerthana-ai123/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 #  git clone ansible-playbook
