e794201cb2eed8f30227901a6ad9065ec289f1ba
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-10-30 16:42:43 +0530

    commit

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -64,11 +64,11 @@ resource "aws_security_group_rule" "catalogue_bastion" {
   to_port           = 22
 }
 
-# resource "aws_security_group_rule" "mongodb_catalogue" {
-#   type              = "ingress"
-#   security_group_id = local.mongodb_sg_id
-#   source_security_group_id = local.catalogue_sg_id
-#   from_port         = 27017
-#   protocol          = "tcp"
-#   to_port           = 27017
-# }
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.catalogue_sg_id
+  from_port         = 27017
+  protocol          = "tcp"
+  to_port           = 27017
+}
