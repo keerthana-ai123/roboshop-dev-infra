dfe40809e6b9d0ebbb7f95ec5af14f00ed685780
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-10-31 15:44:06 +0530

    commit

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -135,7 +135,7 @@ resource "aws_autoscaling_group" "catalogue" {
     version = aws_launch_template.catalogue.latest_version
   }
   vpc_zone_identifier       = local.private_subnet_ids
-  target_group_arns = [aws_lb_target_group.catalogue.arn]
+#   target_group_arns = [aws_lb_target_group.catalogue.arn]
   
   dynamic "tag" {  # we will get the iterator with name as tag
     for_each = merge(
