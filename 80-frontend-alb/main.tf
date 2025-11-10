82179c81c457496c93ed4a6b73e0730af561760a
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-11-10 16:49:14 +0530

    commit

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -34,16 +34,16 @@ resource "aws_lb_listener" "frontend_alb" {
   }
 }
 
-# resource "aws_route53_record" "frontend_alb" {
-#   zone_id = var.zone_id
-#   name    = "roboshop-${var.environment}.${var.domain_name}" # roboshop-dev.daws86s.fun
-#   type    = "A"
-#   allow_overwrite = true
+resource "aws_route53_record" "frontend_alb" {
+  zone_id = var.zone_id
+  name    = "roboshop-${var.environment}.${var.domain_name}" # roboshop-dev.daws86s.fun
+  type    = "A"
+  allow_overwrite = true
 
-#   alias {
-#     # These are ALB details, not our domain details
-#     name                   = aws_lb.frontend_alb.dns_name
-#     zone_id                = aws_lb.frontend_alb.zone_id
-#     evaluate_target_health = true
-#   }
-# }
+  alias {
+    # These are ALB details, not our domain details
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
