375c55160af494d8a5fab1ae6223f9c4da1aa06c
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-11-14 08:29:34 +0530

    commit

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -49,4 +49,6 @@ data "aws_ssm_parameter" "frontend_sg_id" {
   name = "/${var.project_name}/${var.environment}/frontend_sg_id"
 }
 
-//TEST
+data "aws_ssm_parameter" "open_vpn_sg_id" {
+  name = "/${var.project_name}/${var.environment}/open_vpn_sg_id"
+}
